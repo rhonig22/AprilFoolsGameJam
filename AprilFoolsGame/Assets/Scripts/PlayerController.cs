2fbbc8b996383f64cb6e1829d3e12a6489b0932e
rhonig22 <ryan.honig22@outlook.com>
2023-03-28 16:52:04 -0700

    Refactor collision detection
    - fix collision bugs
    - fix jump / grounded logic
    - add raycasting to movement
    - add a little bit to level 2

--- AprilFoolsGame/Assets/Scripts/PlayerController.cs
+++ AprilFoolsGame/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -17,10 +18,13 @@ public class PlayerController : MonoBehaviour
     private float teleportDistance = 3.5f;
     private float teleportTime = .2f;
     private float maxVelocity = 12f;
+    private float deathWaitTime = 1f;
+    private float teleportCooldownTime = 1f;
     private bool facingRight = true;
     private bool canTeleport = true;
     private bool grounded = false;
     private bool isTeleporting = false;
+    private bool isDead = false;
     [SerializeField] Sprite spriteRight;
     [SerializeField] Sprite spriteLeft;
     [SerializeField] Material normalMaterial;
@@ -37,6 +41,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isDead)
+            return;
+
         if (isTeleporting)
         {
             transform.Translate(Vector3.right * (facingRight ? 1 : -1) * teleportDistance * Time.deltaTime / teleportTime);
@@ -62,9 +69,29 @@ public class PlayerController : MonoBehaviour
             spriteRenderer.material= translucent;
             StartCoroutine(EndTeleport());
         }
-        else
+        else if (horizontalInput != 0)
         {
-            transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+            Vector2 boxSize = boxCollider.size;
+            boxSize.y = 0;
+            Vector2 position = playerRB.position + (horizontalInput > 0 ? boxSize / 2 : -boxSize / 2);
+            RaycastHit2D[] hits = Physics2D.RaycastAll(position, Vector3.right * horizontalInput, speed * Time.deltaTime);
+            int index = 0;
+            bool found = false;
+            for (int i = 0; i < hits.Length; i++)
+            {
+                RaycastHit2D hit = hits[i];
+                if (hit.collider != boxCollider && hit.collider != null && hit.rigidbody != null)
+                {
+                    found = true;
+                    index = i;
+                    break;
+                }
+            }
+
+            if (found)
+                transform.Translate(Vector3.right * horizontalInput * hits[index].distance);
+            else
+                transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
         }
 
         // Cap the player's max velocity
@@ -81,57 +108,60 @@ public class PlayerController : MonoBehaviour
         }
 
         // Control jumping
-        if (Input.GetKeyDown(KeyCode.UpArrow) && jumps > 0)
+        if (Input.GetKeyDown(KeyCode.UpArrow) && (grounded || jumps > 0))
         {
             currentVelocity.y = 0;
             playerRB.velocity = currentVelocity;
             playerRB.AddForce(Vector3.up * (grounded ? jumpForce : additionalJumpForce), ForceMode2D.Impulse);
-            jumps--;
+            if (!grounded)
+                jumps--;
             grounded = false;
         }
+    }
 
-        // Control surface collision and grounding logic
-        Collider2D[] hits = Physics2D.OverlapBoxAll(transform.position, boxCollider.size, 0);
-        Vector2 maxMoveDistance = new Vector2(0, 0);
-        foreach (Collider2D hit in hits)
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        CheckGrounding(collision);
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        CheckGrounding(collision);
+    }
+
+    private void CheckGrounding(Collision2D collision)
+    {
+        for (int i = 0; i < collision.contactCount; i++)
         {
-            if (hit == boxCollider)
-                continue;
-
-            ColliderDistance2D colliderDistance = hit.Distance(boxCollider);
-            if (colliderDistance.isOverlapped && !hit.gameObject.GetComponent<Rigidbody2D>().IsUnityNull())
-            {
-                Vector2 moveDistance = colliderDistance.pointA - colliderDistance.pointB;
-                if (moveDistance.magnitude == 0)
-                    continue;
-
-                if (Mathf.Abs(moveDistance.x) > Mathf.Abs(maxMoveDistance.x))
-                    maxMoveDistance.x = moveDistance.x;
-
-                if (Mathf.Abs(moveDistance.y) > Mathf.Abs(maxMoveDistance.y))
-                    maxMoveDistance.y = moveDistance.y;
-            }
-
-            if (Vector2.Angle(colliderDistance.normal, Vector2.up) < 90 && playerRB.velocity.y < 0)
-            {
-                grounded = true;
-                jumps = 1;
-            }
+            Vector2 normal = collision.GetContact(i).normal;
+            grounded |= Vector2.Angle(normal, Vector2.up) < 90;
         }
+    }
 
-        if (maxMoveDistance.magnitude != 0)
-            transform.Translate(maxMoveDistance);
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        grounded = false;
     }
 
     private IEnumerator EndTeleport()
     {
         yield return new WaitForSeconds(teleportTime);
         isTeleporting = false;
+        canTeleport = false;
         spriteRenderer.material = normalMaterial;
+        StartCoroutine(RefreshTeleport());
+    }
+
+    private IEnumerator RefreshTeleport()
+    {
+        yield return new WaitForSeconds(teleportCooldownTime);
+        canTeleport = true;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (isDead)
+            return;
+
         if (collision.gameObject.CompareTag("Spike") && !isTeleporting)
         {
             PlayerDeath();
@@ -151,6 +181,19 @@ public class PlayerController : MonoBehaviour
 
     public void PlayerDeath()
     {
+        isDead= true;
+        Vector2 currentVelocity = playerRB.velocity;
+        currentVelocity.y = 0;
+        playerRB.velocity = currentVelocity;
+        float xForce = UnityEngine.Random.Range(-2f, 2f);
+        playerRB.AddForce(Vector3.up * jumpForce + Vector3.right * xForce, ForceMode2D.Impulse);
+        spriteRenderer.flipY = true;
+        StartCoroutine(Restart());
+    }
+
+    private IEnumerator Restart()
+    {
+        yield return new WaitForSeconds(deathWaitTime);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
